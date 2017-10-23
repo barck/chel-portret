b87b7080acbf966ef3396ee958840cac613b0127
barck94 <artiv09@gmail.com>
2017-10-23 11:26:49 +0500

    3

---
+++ mail.php
@@ -0,0 +1,132 @@
+<html lang="en" class=" js no-touch cssanimations csstransitions">
+<head>
+    <meta charset="utf-8">
+    <title> Поп арт портрет по фото заказать в Челябинске | Art24 | Картины в стиле поп арт на заказ |</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="поп-арт Челябинск портреты попарт челябинск подарок портреты на заказ в Челябинске pop-art popoart мастерская поп-арт портретов в Челябинске" />
+    <meta name="description" content="Изготовление портретов на холсте в стиле поп-арт по фотографии в Челябинске. У нас Вы можете приобрести недорого и быстро. Звоните сейчас! +79630759960" />
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/icomoon.css">
+    <link href="assets/css/animate-custom.css" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/flexslider.css" type="text/css" media="screen" />
+
+    <link href="https://fonts.googleapis.com/css?family=Exo+2:300,700&amp;subset=cyrillic" rel="stylesheet"
+          type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,700&amp;subset=cyrillic" rel="stylesheet"
+          type="text/css">
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body data-spy="scroll" data-offset="0" data-target="#navbar-main">
+
+
+<div id="navbar-main">
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <ul class="nav navbar-nav hidden-lg hidden-sm hidden-xs">
+            <li><h3><span class="mleft10">Портреты в стиле поп-арт по низким ценам в Челябинске</span></h3></li>
+            
+        </ul>
+        <ul class="nav navbar-nav hidden-lg hidden-md hidden-xs">
+            <li><h3><span class="mleft10">Портреты в стиле поп-арт в Челябинске</span></h3></li>
+            
+        </ul>
+        <ul class="nav navbar-nav hidden-lg hidden-md hidden-sm">
+            
+        </ul>
+        <div class="container hidden-md hidden-sm hidden-xs">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <img src="assets/img/logo.png" alt="поп-арт портрет Челябинск" title="поп-арт портрет Челябинск" width="80px">
+                </button>
+                <a class="navbar-brand hidden-xs hidden-sm" href="index.html"><img src="assets/img/logo.png"
+                                                                                   alt="поп-арт портрет на заказ"
+                                                                                   title="поп-арт портрет на заказ"
+                                                                                   width="130px" class="logoround"></a>
+            </div>
+            <div class="navbar-collapse collapse marginmenu">
+                <ul class="nav navbar-nav">
+                    
+                </ul>
+                <h3 class="text-right-n"><span class="icon icon-phone icon1"
+                                               style="font-size:26px; color:#ff953d;"></span>+7 (963) 075-99-60</h3>
+            </div><!--/.nav-collapse -->
+        </div>
+    </div>
+</div>
+
+
+<!-- ==== HEADERWRAP ==== -->
+<div id="headerwrap" name="home">
+    <header class="clearfix">
+        <div class="col-sm-12 hidden-lg hidden-md hidden-xs">
+            <h2 class="margindown"><span class="back_name">ПОП-АРТ ПОРТРЕТ</span><br><span class="back2_name">СТИЛЬНЫЙ, МОДНЫЙ, ОРИГИНАЛЬНЫЙ ПОДАРОК</span></h2>
+            <img src="assets/img/pop-art1.png" width="95%" alt="Поп арт портрет" title="Поп арт портрет">
+        </div>
+        <div class="col-xs-12 320img hidden-lg hidden-sm hidden-md">
+            <h2 class="margindown"><span class="back_name">ПОП-АРТ ПОРТРЕТ</span><br><span class="back2_name">СТИЛЬНЫЙ, МОДНЫЙ, ОРИГИНАЛЬНЫЙ ПОДАРОК</span></h2>
+            <img src="assets/img/pop-art1.png" width="95%" alt="Поп арт портрет" title="Поп арт портрет">
+        </div>
+        <div class="container2 hidden-sm hidden-xs">
+            <div class="col-md-7 mleft111 margindown hidden-md">
+                <img src="assets/img/pop-art1.png" width="100%" alt="Поп арт портрет" title="Поп арт портрет">
+            </div>
+            <div class="col-md-7 mleft111 marginup hidden-lg">
+                <img src="assets/img/pop-art1.png" width="100%" alt="Поп арт портрет" title="Поп арт портрет">
+            </div>
+            <div class="col-md-5 mright111">
+                <h1 class="hidden-md"><span class="back_name">ПОП-АРТ ПОРТРЕТ</span><br><span class="back2_name">СТИЛЬНЫЙ, МОДНЫЙ, ОРИГИНАЛЬНЫЙ ПОДАРОК</span>
+                </h1>
+                <h2 class="hidden-lg"><span class="back_name">ПОП-АРТ ПОРТРЕТ</span><br><span class="back2_name">СТИЛЬНЫЙ, МОДНЫЙ, ОРИГИНАЛЬНЫЙ ПОДАРОК</span>
+                </h2>
+                
+                
+            </div>
+        </div>
+    </header>
+</div><!-- /headerwrap -->
+
+
+</div><!-- greywrap -->
+
+<script src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/retina.js"></script>
+<script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="assets/js/smoothscroll.js"></script>
+<script type="text/javascript" src="assets/js/jquery-func.js"></script>
+<script type="text/javascript" src="assets/js/selectsize.js"></script>
+<script type="text/javascript" src="assets/js/selectkind.js"></script>
+
+<!-- FlexSlider -->
+  <script defer src="assets/js/jquery.flexslider.js"></script>
+
+  <script type="text/javascript">
+    $(function(){
+      SyntaxHighlighter.all();
+    });
+    $(window).load(function(){
+      $('.flexslider').flexslider({
+        animation: "slide",
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+      });
+    });
+  </script>
+<center>
+</body>
+</html>
+</center>
